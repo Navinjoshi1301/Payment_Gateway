b203279583b14e225eafd6feca05fc78d06b2a5f
Navin joshi <115449678+Navinjoshi1301@users.noreply.github.com>
2024-05-28 17:27:01 +0530

    Update PaytmApplication.java

--- paytm/src/main/java/com/paytm/paytm/PaytmApplication.java
+++ paytm/src/main/java/com/paytm/paytm/PaytmApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PaytmApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PaytmApplication.class, args);
 	}
